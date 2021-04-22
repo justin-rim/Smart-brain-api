fdd99d1c5796c6cc550ee42d0a5a60a5cbba7744
Justin <jeuirim@gmail.com>
2021-04-22 13:30:43 -0700

    adding changes to '/'

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req,res) => {res.send(database.users)})
+app.get('/', (req,res) => {res.send('It is working')})
 app.post('/signin',  signin.handleSignIn( db, bcrypt))
 app.post('/register', register.handleRegister( db, bcrypt))
 app.get('/profile/:id', profile.handleProfile( db))
