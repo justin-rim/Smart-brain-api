c633a0bb07d16611be46e791385baca2045e04c6
Justin <jeuirim@gmail.com>
2021-04-23 13:15:51 -0700

    changes to database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-octagonal-85539',
-    user : 'justinrim',
-    password : '',
-    database : 'smart_brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
